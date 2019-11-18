2241a888de6d84ba72baf6968b526d26655c5112
Ryan Anderson <randerson@rapdevapp.com>
2019-11-18 15:57:09 -0600

    Add MVC to project

--- EmployeeManagement.Mvc/EmployeeManagement.Mvc/Startup.cs
+++ EmployeeManagement.Mvc/EmployeeManagement.Mvc/Startup.cs
@@ -23,6 +23,7 @@ namespace EmployeeManagement.Mvc
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddMvcCore();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,12 +34,9 @@ namespace EmployeeManagement.Mvc
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.UseFileServer();
+			app.UseStaticFiles();
 
-			app.Use(async (context, next) =>
-			{
-				await context.Response.WriteAsync("Hello world.");
-			});
+			app.UseMvcWithDefaultRoute();
 		}
 	}
 }
