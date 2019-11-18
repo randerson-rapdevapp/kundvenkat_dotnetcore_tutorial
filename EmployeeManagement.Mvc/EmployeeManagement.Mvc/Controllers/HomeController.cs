65ea1a5f651710d8305533da90569881673eedca
Ryan Anderson <randerson@rapdevapp.com>
2019-11-18 16:48:59 -0600

    Add initial employee managment controller/model

---
+++ EmployeeManagement.Mvc/EmployeeManagement.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+		private IEmployeeRepository _employeeRepository;
+
+		public HomeController(IEmployeeRepository employeeRepository)
+		{
+			_employeeRepository = employeeRepository;
+		}
+
+        public string Index()
+        {
+			return _employeeRepository.GetEmployee(1).Name;
+        }
+    }
+}
---
+++ EmployeeManagement.Mvc/EmployeeManagement.Mvc/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Mvc.Models
+{
+	public class MockEmployeeRepository : IEmployeeRepository
+	{
+		private List<Employee> _employeeList;
+
+		public MockEmployeeRepository()
+		{
+			_employeeList = new List<Employee>
+			{
+				new Employee() { id=1, Name="Ryan", Department = "HR", Email="ryan@gmail.com"},
+				new Employee() { id = 2, Name = "Michelle", Department = "IT", Email="michelle@gmail.com" },
+				new Employee() { id = 3, Name= "Tammy", Department = "Sales", Email="tammy@gmail.com" }
+			};
+		}
+
+		public Employee GetEmployee(int id)
+		{
+			return _employeeList.FirstOrDefault(f => f.id.Equals(id));
+		}
+	}
+}
