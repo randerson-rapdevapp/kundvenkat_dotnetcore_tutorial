3f7bce54456abc6685f7af6b581112e0458b8eb6
Ryan Anderson <randerson@rapdevapp.com>
2019-11-18 13:38:33 -0600

    Test developer exceptions

--- EmployeeManagement.Mvc/EmployeeManagement.Mvc/Startup.cs
+++ EmployeeManagement.Mvc/EmployeeManagement.Mvc/Startup.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement.Mvc
 {
 	public class Startup
 	{
+		private IConfiguration _config;
+
+		public Startup(IConfiguration configuration)
+		{
+			_config = configuration;
+		}
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
@@ -25,9 +33,15 @@ namespace EmployeeManagement.Mvc
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.Run(async (context) =>
+			var fileServerOptions = new FileServerOptions();
+			fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+			fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("home.html");
+			app.UseFileServer(fileServerOptions);
+
+			app.Use(async (context, next) =>
 			{
-				await context.Response.WriteAsync("Hello World!");
+				throw new Exception("blah");
+				await context.Response.WriteAsync("Hello world.");
 			});
 		}
 	}
